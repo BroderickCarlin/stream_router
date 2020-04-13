f1716a3474c16abadee790d1a57c2edef0100b39
Broderick Carlin <broderick.carlin@gmail.com>
2020-04-13 10:18:23 -0500

    Fix stateful example and add dedup example

--- examples/dedup.rs
+++ examples/dedup.rs
@@ -3,7 +3,6 @@ use std::sync::Arc;
 use tokio;
 use num_stream::num_stream;
 use futures::{
-    future,
     lock::Mutex,
     future::FutureExt,
     sink,
@@ -13,32 +12,32 @@ use futures::{
 use stream_router;
 
 struct State {
-    count: u64,
-    output: bool
+    val: u64
 }
 
 #[tokio::main]
 async fn main() {
-    let nums = num_stream(0, 1, Duration::from_millis(50));
+    let nums1 = num_stream(0, 1, Duration::from_millis(50));
+    let nums2 = num_stream(0, 2, Duration::from_millis(100));
     let black_hole = sink::drain().sink_map_err(|_| ());
-    let state = Arc::new(Mutex::new(State{count: 0, output: true}));
+    let state = Arc::new(Mutex::new(State{val: 0}));
 
-    let is_enabled = move |_x| {
+    
+    let is_dup = move |x| {
         let state = state.clone();
         async move {
             state.lock().map(|mut state|{
-                if state.count % 10 == 0 {
-                    state.output ^= true;
-                }
-                state.count += 1;
-                state.output
+                let is_dup = state.val == x;
+                state.val = x;
+                is_dup
             }).await
         }.boxed()
     };
 
     let mut router = stream_router::StreamRouter::new();
-    router.add_source(nums, is_enabled);
-    router.add_sink(black_hole, false);
+    router.add_source(nums1, is_dup.clone());
+    router.add_source(nums2, is_dup);
+    router.add_sink(black_hole, true);
     
     loop {
         let val = router.next().await;
