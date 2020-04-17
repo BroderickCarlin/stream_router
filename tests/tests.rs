a5283d4034f2444b5abd2721f054d87394f008f4
Broderick Carlin <broderick.carlin@gmail.com>
2020-04-17 14:17:46 -0500

    rustfmt tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,11 +1,11 @@
-use stream_router::StreamRouter;
-use futures::task::Poll;
-use futures::stream::{self, Stream};
-use futures::sink::drain;
 use futures::future::ready;
-use futures_test::task::noop_context;
+use futures::sink::drain;
+use futures::stream::{self, Stream};
+use futures::task::Poll;
 use futures_test::stream::StreamTestExt;
+use futures_test::task::noop_context;
 use std::pin::Pin;
+use stream_router::StreamRouter;
 
 #[test]
 fn single_poll_no_routing() {
@@ -14,9 +14,15 @@ fn single_poll_no_routing() {
     let mut router = StreamRouter::new();
 
     router.add_source(nums, |x| ready((x, ())));
-    
-    assert_eq!(Pin::new(&mut router).poll_next(&mut cx), Poll::Ready(Some(1)));
-    assert_eq!(Pin::new(&mut router).poll_next(&mut cx), Poll::Ready(Some(2)));
+
+    assert_eq!(
+        Pin::new(&mut router).poll_next(&mut cx),
+        Poll::Ready(Some(1))
+    );
+    assert_eq!(
+        Pin::new(&mut router).poll_next(&mut cx),
+        Poll::Ready(Some(2))
+    );
     assert_eq!(Pin::new(&mut router).poll_next(&mut cx), Poll::Ready(None));
 }
 
@@ -27,11 +33,17 @@ fn handling_pending_no_routing() {
     let mut router = StreamRouter::new();
 
     router.add_source(nums, |x| ready((x, ())));
-    
+
     assert_eq!(Pin::new(&mut router).poll_next(&mut cx), Poll::Pending);
-    assert_eq!(Pin::new(&mut router).poll_next(&mut cx), Poll::Ready(Some(1)));
+    assert_eq!(
+        Pin::new(&mut router).poll_next(&mut cx),
+        Poll::Ready(Some(1))
+    );
     assert_eq!(Pin::new(&mut router).poll_next(&mut cx), Poll::Pending);
-    assert_eq!(Pin::new(&mut router).poll_next(&mut cx), Poll::Ready(Some(2)));
+    assert_eq!(
+        Pin::new(&mut router).poll_next(&mut cx),
+        Poll::Ready(Some(2))
+    );
     assert_eq!(Pin::new(&mut router).poll_next(&mut cx), Poll::Pending);
     assert_eq!(Pin::new(&mut router).poll_next(&mut cx), Poll::Ready(None));
 }
@@ -51,8 +63,14 @@ fn single_poll_routing() {
     router.add_source(nums, is_even);
     router.add_sink(black_hole, false);
 
-    assert_eq!(Pin::new(&mut router).poll_next(&mut cx), Poll::Ready(Some(2)));
-    assert_eq!(Pin::new(&mut router).poll_next(&mut cx), Poll::Ready(Some(4)));
+    assert_eq!(
+        Pin::new(&mut router).poll_next(&mut cx),
+        Poll::Ready(Some(2))
+    );
+    assert_eq!(
+        Pin::new(&mut router).poll_next(&mut cx),
+        Poll::Ready(Some(4))
+    );
     assert_eq!(Pin::new(&mut router).poll_next(&mut cx), Poll::Ready(None));
 }
 
@@ -73,12 +91,18 @@ fn handling_pending_routing() {
 
     assert_eq!(Pin::new(&mut router).poll_next(&mut cx), Poll::Pending);
     // Value `1` yielded to `black_hole` on the next call TO `poll_next`
-    assert_eq!(Pin::new(&mut router).poll_next(&mut cx), Poll::Pending); 
-    assert_eq!(Pin::new(&mut router).poll_next(&mut cx), Poll::Ready(Some(2)));
+    assert_eq!(Pin::new(&mut router).poll_next(&mut cx), Poll::Pending);
+    assert_eq!(
+        Pin::new(&mut router).poll_next(&mut cx),
+        Poll::Ready(Some(2))
+    );
     assert_eq!(Pin::new(&mut router).poll_next(&mut cx), Poll::Pending);
     // Value `3` yielded to `black_hole` on the next call to `poll_next`
-    assert_eq!(Pin::new(&mut router).poll_next(&mut cx), Poll::Pending); 
-    assert_eq!(Pin::new(&mut router).poll_next(&mut cx), Poll::Ready(Some(4)));
+    assert_eq!(Pin::new(&mut router).poll_next(&mut cx), Poll::Pending);
+    assert_eq!(
+        Pin::new(&mut router).poll_next(&mut cx),
+        Poll::Ready(Some(4))
+    );
     assert_eq!(Pin::new(&mut router).poll_next(&mut cx), Poll::Pending);
     assert_eq!(Pin::new(&mut router).poll_next(&mut cx), Poll::Ready(None));
 }
@@ -90,14 +114,12 @@ fn single_poll_forward() {
     let mut router = stream_router::StreamRouter::new();
     let black_hole = drain();
 
-    let is_even = |x| {
-        ready((x, true))
-    };
+    let is_even = |x| ready((x, true));
 
     router.add_source(nums, is_even);
     router.add_sink(black_hole, true);
 
     // This single call to `poll_next` will forward all value into `black_hole`
-    // and exhaust the input `Stream`. 
+    // and exhaust the input `Stream`.
     assert_eq!(Pin::new(&mut router).poll_next(&mut cx), Poll::Ready(None));
 }
